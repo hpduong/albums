41ddb62dd9a34a3784917c420fa87a2a37af8ce7
hpduong <henryduong@gmail.com>
2017-07-18 15:58:39 -0700

    render array from get request

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
-const AlbumList = () => {
-  return (
-    <View>
-      <Text> Album List!!!!</Text>
-    </View>
-  );
-};
+
+class AlbumList extends Component { // this is a Dynamic Class Component vs Statis Functional Component
+  state = { albums: [] }; //empty array
+
+  componentWillMount() { //runs js as soon as AlbumList is called
+    fetch('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => response.json())
+      .then(data => this.setState({ albums: data })); //setState tells to re-render with new data, updates component state
+   }
+
+  renderAlbums() {
+    return this.state.albums.map(album => <Text>{album.title}</Text>);
+  }
+
+  render() {
+    console.log(this.state);
+
+    return (
+      <View>
+        {this.renderAlbums()}
+        </View>
+      );
+    }
+  }
 
 export default AlbumList;
